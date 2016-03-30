1ce4bc9f94601bf77641292b385b3ef0a735fb29
Medusalix <ss7@live.de>
2016-03-30 12:45:06 +0200

    Migrate from Jackson to Gson

--- src/main/java/de/medusalix/biblios/core/GoogleBooks.java
+++ src/main/java/de/medusalix/biblios/core/GoogleBooks.java
@@ -1,15 +1,16 @@
 package de.medusalix.biblios.core;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import de.medusalix.biblios.pojos.GoogleBook;
 import de.medusalix.biblios.pojos.GoogleBookQuery;
 import de.medusalix.biblios.utils.ExceptionUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,6 +19,8 @@ import java.util.List;
 public class GoogleBooks
 {
     private static Logger logger = LogManager.getLogger(GoogleBooks.class);
+
+    private static Gson gson = new GsonBuilder().create();
 
     public static String readApiKey()
     {
@@ -56,21 +59,24 @@ public class GoogleBooks
     {
         String apiKey = readApiKey();
 
-        try (InputStream queryInputStream = new URL(String.format(Consts.GoogleBooks.GET_VOLUMES_BY_ISBN_URL, isbn, apiKey)).openStream())
+        try
         {
-            ObjectMapper mapper = new ObjectMapper();
+            URL volumeListUrl = new URL(String.format(Consts.GoogleBooks.GET_VOLUMES_BY_ISBN_URL, isbn, apiKey));
 
-            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-            List<GoogleBook> items = mapper.readValue(queryInputStream, GoogleBookQuery.class).getItems();
-
-            if (items != null)
+            try (BufferedReader volumeListReader = new BufferedReader(new InputStreamReader(volumeListUrl.openStream())))
             {
-                try (InputStream volumeInputStream = new URL(String.format(Consts.GoogleBooks.VOLUME_INFO_FIELDS_URL, items.get(0).getSelfLink(), apiKey)).openStream())
-                {
-                    logger.info("Google Books information fetched");
+                List<GoogleBook> items = gson.fromJson(volumeListReader, GoogleBookQuery.class).getItems();
 
-                    return mapper.readValue(volumeInputStream, GoogleBook.class).getVolumeInfo();
+                if (items != null)
+                {
+                    URL volumeUrl = new URL(String.format(Consts.GoogleBooks.VOLUME_INFO_FIELDS_URL, items.get(0).getSelfLink(), apiKey));
+
+                    try (BufferedReader volumeReader = new BufferedReader(new InputStreamReader(volumeUrl.openStream())))
+                    {
+                        logger.info("Google Books information fetched");
+
+                        return gson.fromJson(volumeReader, GoogleBook.class).getVolumeInfo();
+                    }
                 }
             }
         }
