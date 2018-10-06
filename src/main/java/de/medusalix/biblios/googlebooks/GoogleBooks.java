a58f96db84604d85d7913de77d5d8e8e280bd8ad
Medusalix <ss7@live.de>
2018-10-06 17:24:19 +0200

    Clean up GoogleBooks interface

--- src/main/java/de/medusalix/biblios/googlebooks/GoogleBooks.java
+++ src/main/java/de/medusalix/biblios/googlebooks/GoogleBooks.java
@@ -18,8 +18,7 @@ package de.medusalix.biblios.googlebooks;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import de.medusalix.biblios.core.Consts;
-import de.medusalix.biblios.utils.FileUtils;
+import de.medusalix.biblios.Consts;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,95 +26,86 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class GoogleBooks
 {
     private static final Logger logger = LogManager.getLogger(GoogleBooks.class);
     
     private static Gson gson = new GsonBuilder().create();
-    
-    public static void findVolumeInfoForIsbn(String isbn, Consumer<VolumeInfo> callback)
+
+    public static void query(String isbn, Consumer<VolumeInfo> callback)
     {
-        new Thread(() ->
+        CompletableFuture.supplyAsync(new QueryTask(isbn))
+            .thenAccept(callback);
+    }
+
+    private static List<GoogleBook> findBooksForIsbn(String apiKey, String isbn) throws IOException
+    {
+        URL url = new URL(String.format(Consts.GoogleBooks.VOLUMES_BY_ISBN_URL, isbn, apiKey));
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream())))
+        {
+            return gson.fromJson(reader, Query.class).getItems();
+        }
+    }
+
+    private static VolumeInfo findVolumeInfoForBook(String apiKey, GoogleBook book) throws IOException
+    {
+        URL url = new URL(String.format(Consts.GoogleBooks.VOLUME_INFO_FIELDS_URL, book.getSelfLink(), apiKey));
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream())))
+        {
+            return gson.fromJson(reader, GoogleBook.class).getVolumeInfo();
+        }
+    }
+
+    public static class QueryTask implements Supplier<VolumeInfo>
+    {
+        private String isbn;
+
+        public QueryTask(String isbn)
+        {
+            this.isbn = isbn;
+        }
+
+        @Override
+        public VolumeInfo get()
         {
             try
             {
-                String apiKey = FileUtils.readLine(Consts.Paths.API_KEY);
-                
-                List<Query.GoogleBook> books = findBooksForIsbn(apiKey, isbn);
-                
+                List<String> lines = Files.readAllLines(Consts.Paths.API_KEY);
+                String apiKey = lines.get(0);
+
+                List<GoogleBook> books = findBooksForIsbn(apiKey, isbn);
+
                 if (books == null || books.size() == 0)
                 {
                     logger.info("No books found for ISBN " + isbn);
-    
-                    callback.accept(null);
-    
-                    return;
+
+                    return null;
                 }
-                
+
                 logger.info("Found " + books.size() + " books for ISBN " + isbn);
-                
+
                 // Use the first result and fetch the volume info
                 VolumeInfo volumeInfo = findVolumeInfoForBook(apiKey, books.get(0));
-                
+
                 logger.info("Using first result: " + volumeInfo);
-                        
-                callback.accept(volumeInfo);
+
+                return volumeInfo;
             }
 
             catch (IOException e)
             {
                 logger.error("", e);
             }
-        }).start();
-    }
-    
-    private static List<Query.GoogleBook> findBooksForIsbn(String apiKey, String isbn) throws IOException
-    {
-        URL url = new URL(String.format(Consts.GoogleBooks.VOLUMES_BY_ISBN_URL, isbn, apiKey));
-    
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream())))
-        {
-            return gson.fromJson(reader, Query.class).getItems();
-        }
-    }
-    
-    private static VolumeInfo findVolumeInfoForBook(String apiKey, Query.GoogleBook book) throws IOException
-    {
-        URL url = new URL(String.format(Consts.GoogleBooks.VOLUME_INFO_FIELDS_URL, book.getSelfLink(), apiKey));
-    
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream())))
-        {
-            return gson.fromJson(reader, Query.GoogleBook.class).getVolumeInfo();
-        }
-    }
-    
-    private static class Query
-    {
-        private List<GoogleBook> items;
-        
-        public List<GoogleBook> getItems()
-        {
-            return items;
-        }
-        
-        private static class GoogleBook
-        {
-            private String selfLink;
-    
-            private VolumeInfo volumeInfo;
-            
-            public String getSelfLink()
-            {
-                return selfLink;
-            }
-            
-            public VolumeInfo getVolumeInfo()
-            {
-                return volumeInfo;
-            }
+
+            return null;
         }
     }
 }
